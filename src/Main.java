4ece8fff064e9440870f447c9b6b3e31fcbd980c
katherinechmelev <katherinec12@nycstudents.net>
2022-10-11 12:32:36 -0400

    not done

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+public static void main(String [] args){
+    String str1 = "Hello!";
+    String str2 = "Hello!";
+    String str3 = new String("Hello!");
+    String str4 = new String("Hello!");
+
+    System.out.println(str1 == str2);
+    System.out.println(str1 == str3);
+    System.out.println(str1 == str4);
+    System.out.println(str3 == str4);
+
+
+
+
+
+}
+}
