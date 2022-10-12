fa668df57b228fb4e0e92948efb3dc4e187068e2
Katherine <katherinechmelev@gmail.com>
2022-10-11 21:47:56 -0400

    done

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,49 @@
+import java.util.Scanner;
+
 public class Main {
 public static void main(String [] args){
-    String str1 = "Hello!";
-    String str2 = "Hello!";
-    String str3 = new String("Hello!");
-    String str4 = new String("Hello!");
 
-    System.out.println(str1 == str2);
-    System.out.println(str1 == str3);
-    System.out.println(str1 == str4);
-    System.out.println(str3 == str4);
+    Scanner scan = new Scanner(System.in);
+
+    System.out.print("Enter first string: ");
+    String str1 = scan.nextLine();
+
+    int lenStr1 = str1.length();
+    System.out.println("String length: " + lenStr1);
+
+    int firstHalf = lenStr1/2;
+
+    System.out.println("First half: " + str1.substring(0, firstHalf));
+    System.out.println("Second half: " + str1.substring(firstHalf));
+
+
+    System.out.print("Enter second string: ");
+    String str2= scan.nextLine();
+    int lenStr2 = str2.length();
+
+    if (lenStr1 > lenStr2){
+        System.out.println(str1 + " is longer");
+    }else if(lenStr1 < lenStr2){
+        System.out.println(str2 + " is longer");
+    }else if (lenStr1 == lenStr2){
+        System.out.println("Both strings have the same length.");
+    }
+
+    if (str1.equals(str2)){
+        System.out.println("Both strings have the exact same characters.");
+    }else if (str1.compareTo(str2) < 0) {
+        System.out.println(str1 + " is first alphabetically");
+    }else if(str1.compareTo(str2) > 0) {
+        System.out.println(str2 + " is first alphabetically");
+    }
+    int indexOfSecondString = str1.indexOf(str2);
+    if (indexOfSecondString != -1){
+        System.out.println(str2 + " is found in " + str1 + " at index " + indexOfSecondString);
+    }else{
+        System.out.println(str2 + " is NOT found in " + str1);
+    }
+
+
 
 
 
